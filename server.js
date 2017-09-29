fd6cb2a80ed3118736cae237e1c0af7b974fbe37
Alex Foxleigh <alex@volcaniccreations.com>
2017-09-29 19:20:04 +0100

    No portfolio files yet, just setting the project up and ensuring deployment runs

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
-var basicAuth = require('express-basic-auth')
+// var basicAuth = require('express-basic-auth')
 
 var port = process.env.PORT || 5000
 
-app.use(basicAuth({
-  users: { 'admin': process.env.AUTH_KEY },
-  challenge: true
-}))
+// app.use(basicAuth({
+//   users: { 'admin': process.env.AUTH_KEY },
+//   challenge: true
+// }))
 
 app.use(express.static(__dirname + '/dist'))
 
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const RobotstxtPlugin = require('robotstxt-webpack-plugin').default
+
+const options = {
+  policy: [
+    {
+      userAgent: '*',
+      disallow: '/'
+    }
+  ]
+}
+
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      // Process css styles
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      // Process images
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      // Process fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new RobotstxtPlugin(options),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ]
+}
+
+
