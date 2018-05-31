ae27ede3fd72e7bfa459b3f548c0647a2e54a9ec
Alex Foxleigh <alex@alexfoxleigh.com>
2018-05-31 16:26:32 +0100

    Added SiteHeader component Closes #14 - Added site header - Added app component as well as there wasn't a wrapper set up

---
+++ src/components/presentational/SiteHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const SiteHeader = (props) => (
+  <header>
+    This is where the logo goes
+    {/* This is where the site nav will go */}
+  </header>
+)
+
+export default SiteHeader
