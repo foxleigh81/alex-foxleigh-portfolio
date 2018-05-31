d11212b80db273322cdafea41af58b8499ee67e4
Alex Foxleigh <alex@alexfoxleigh.com>
2018-05-31 16:26:32 +0100

    Added SiteHeader component Closes #14 - Added site header - Added app component as well as there wasn't a wrapper set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import Example from './components/container/example/ExampleContainer'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+// Import components
+import SiteHeader from './components/presentational/SiteHeader'
+
+class App extends Component {
+  constructor () {
+    super()
+  }
+  render () {
+    return (
+      <div>
+        <SiteHeader />
+        <main>
+          <h1>Main content goes here</h1>
+        </main>
+      </div>
+    )
+  }
+}
+export default App
+
+const wrapper = document.getElementById('appRoot')
+wrapper ? ReactDOM.render(<App />, wrapper) : false
