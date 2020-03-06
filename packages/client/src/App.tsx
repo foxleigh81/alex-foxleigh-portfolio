753d8f24150292637b608fcd1a21af15c41ce9b4
Alex Foxleigh <alex@alexfoxleigh.com>
2020-03-06 23:36:21 +0000

    added routing and set up initial route for home page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
-import logo from './logo.svg';
 import './App.css';
  
  class App extends Component {
@@ -9,16 +8,6 @@ import './App.css';
      return (
       <BrowserRouter>
          <div className="App">
-           <header className="App-header">
-            <Link to="/">
-               <img src={logo} className="App-logo" alt="logo" />
-            </Link>
-             <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link> <Link to="/post/">Post</Link>
-           </header>
--        <p className="App-intro">
--          To get started, edit <code>src/App.js</code> and save to reload.
--        </p>
           <Switch>
             <Route exact path="/" component={Home} />
             {/* <Route path="/post/" component={Post} /> */}
