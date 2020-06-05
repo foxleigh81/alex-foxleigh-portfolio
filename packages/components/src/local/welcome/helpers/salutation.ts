7a2880ff3f1cb9554f9faa3f5728bb4383932383
Alex Foxleigh <alex@alexfoxleigh.com>
2020-06-05 23:09:48 +0100

    Added styles to welcome component and tweaked the salutation helper to work better - also updated stories

--- packages/components/src/local/welcome/helpers/salutation.ts
+++ packages/components/src/local/welcome/helpers/salutation.ts
@@ -1,29 +1,72 @@
 import moment from 'moment'
 
-const salutation = () => {
+const salutation = (hour?: number) => {
     moment.locale()
-    const hour = parseInt(moment().format('HH'))
     
+    const time = hour || moment().hour()
+    
+    const timeOfDay = (t: number): string => {
+
+        let timeString = 'night'
+
+        // If it's between 23:00 and 06:00 then it's night time
+        if (t > 22 || t < 5) timeString = 'night'
+        // If it's between 06:00 and 09:00 then it's early morning
+        if (t >= 5 && t < 10) timeString = 'early-morning'
+        // If it's between 10:00 and 11:00 then it's mid morning
+        if (t >= 10 && t < 12) timeString = 'mid--morning'
+        // If it's between 12:00 and 18:00 then it's the afternoon
+        if (t >= 12 && t < 18) timeString = 'afternoon'
+        // If it's between 19L00 and 22:00 then it's the evening
+        if (t >= 18 && t < 22) timeString = 'evening'
+
+        return timeString
+    }
+
     // Determine which segment of the day it is
     let segment = (t: number): string => {
-        if (t < 12) return 'morning'
-        if (t >= 12 && t <= 17) return 'afternoon'
-        if (t >= 18) return 'evening'        
-        return 'night'
+        switch(timeOfDay(time)) {
+            case 'early-morning':
+            case 'mid-morning':
+                return `Good morning!`
+                break
+            case 'afternoon':
+            default:
+                return `Good Afternoon!`
+                break
+            case 'evening':
+                return `Good evening!`
+                break
+            case 'night':
+                return `Hi, you're up late!`
+                break
+        }
     }
 
     // Determine an approximate light level
     let lightLevel = (t: number): string => {
-        if (t > 10 && t < 6) return 'darkness'
-        if (t >= 18 && t <= 21) return 'sunset'        
-        if (t >= 6 && t <= 9) return 'sunrise'        
-        return 'daylight'
+        switch(timeOfDay(time)) {
+            case 'early-morning':
+                return 'sunrise'
+                break
+            case 'mid-morning':
+            case 'afternon':
+            default: 
+                return 'daylight'
+                break
+            case 'evening':
+                return 'sunset'
+                break
+            case 'night':
+                return 'darkness'
+                break
+        }
     }
 
     return { 
-        segment: segment(hour),
-        lightLevel: lightLevel(hour)
+        segment: segment(time),
+        lightLevel: lightLevel(time)
     }
 }
 
-export default salutation
+export default salutation;
--- packages/components/src/local/welcome/index.stories.tsx
+++ packages/components/src/local/welcome/index.stories.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import moment from 'moment'
+import { withKnobs, number } from '@storybook/addon-knobs'
 
 // Import component files
 import Welcome from './index'
@@ -8,7 +10,7 @@ import docs from './README.md'
 export default {
   title: 'local/Welcome',
   component: Welcome,
-  decorators: [],
+  decorators: [withKnobs],
   parameters: {
     readme: {
       // Show readme before story
@@ -17,7 +19,17 @@ export default {
   }
 }
 
-export const Available: React.FC = () => <Welcome status="available" minime="available" />
-export const Unavailable: React.FC = () => <Welcome status="unavailable" minime="at-desk" />
-export const OnHoliday: React.FC = () => <Welcome status="holiday" minime="smile" />
+const label = 'Current hour (24h)';
+const defaultValue = parseInt(moment().format('HH'));
+const options = {
+   range: true,
+   min: 0,
+   max: 23,
+   step: 1,
+};
+ 
+export const Available: React.FC = () => <Welcome status="available" nextDate="25/12/20" minime="available" hour={number(label, defaultValue, options)} />
+export const Unavailable: React.FC = () => <Welcome status="unavailable" nextDate="25/12/20" minime="at-desk" hour={number(label, defaultValue, options)}/>
+export const UnavailableNoReturnDate: React.FC = () => <Welcome status="unavailable" minime="at-desk" hour={number(label, defaultValue, options)}/>
+export const OnHoliday: React.FC = () => <Welcome status="holiday" nextDate="25/12/20" minime="smile" hour={number(label, defaultValue, options)} />
 
