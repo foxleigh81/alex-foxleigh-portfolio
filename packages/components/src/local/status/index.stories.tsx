de157c1b84181cf7393ba3fbc9cdc76424625463
Alex Foxleigh <alex@alexfoxleigh.com>
2020-06-05 23:09:16 +0100

    Updated status component to allow for date to be optional

--- packages/components/src/local/status/index.stories.tsx
+++ packages/components/src/local/status/index.stories.tsx
@@ -18,5 +18,6 @@ export default {
 
 export const Available: React.FC = () => <Status status='available' />
 export const Unavailable: React.FC = () => <Status status='unavailable' date="2/12/81" />
+export const UnavailableNoReturnDate: React.FC = () => <Status status='unavailable' />
 export const OnHoliday: React.FC = () => <Status status='holiday' date="2/12/81" />
 
