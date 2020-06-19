a715a85d7294f21e8763f582315daf76a6aa47d4
Alex Foxleigh <alex@alexfoxleigh.com>
2020-06-19 18:19:36 +0100

    Updates! - Added office bg for the unavailable status - Added a new 'foreground' DOM element to accomodate multiple backgrounds - Tweaked styles on backgrounds to make it work properly - Added a buttonColor mixin to allow for different button styles

--- packages/components/src/welcome/index.tsx
+++ packages/components/src/welcome/index.tsx
@@ -19,14 +19,16 @@ import Status from '../status'
 export const Welcome: React.FC<Props> = ({ status, nextDate, miniMeName, miniMeWidth, hour }: Props) => { 
  const { segment, lightLevel } = salutation(hour || undefined)
  return <div className={cx(styles['block'], styles[lightLevel], styles[`status-${status}`])}>
+   <div className={styles.foreground}>
     <MiniMe name={miniMeName} width={miniMeWidth || 'regular'} classes={"mini-me"}/>
-    <div className={styles["salutation"]}>
-      <h1>{capitalize(segment)}</h1>
-      <h2>I'm Alex, I like to make things</h2>
-    </div>
-    <div className={styles['status-container']}>
-      <Status status={status} date={nextDate} />
-    </div>
+      <div className={styles["salutation"]}>
+        <h1>{capitalize(segment)}</h1>
+        <h2>I'm Alex, I like to make things</h2>
+      </div>
+      <div className={styles['status-container']}>
+        <Status status={status} date={nextDate} />
+      </div>
+   </div>
   </div>
 }
 
