3ab450d7768b93e752110b489ad755a7c40a2ad3
Alexander Foxleigh <alex@alexfoxleigh.com>
2021-10-14 16:04:10 +0100

    Updated availability

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import styles from './styles.module.scss'
 /* Render component */
 export const Home: React.FC = () =>
   <div className={styles['page']}>
-    <Welcome status="available" nextDate="25/12/20" miniMeName="available" />
+    <Welcome status="unavailable" nextDate="01/03/22" miniMeName="unavailable" />
   </div>
 
 export default Home
