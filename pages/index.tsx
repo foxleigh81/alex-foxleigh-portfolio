a5b156542419ae21a0e0679a882a600a47d66b14
Alexander Foxleigh <alex@alexfoxleigh.com>
2021-10-14 16:11:16 +0100

    Updated the mini-me name as I'd forgotten it was called 'at-desk'

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import styles from './styles.module.scss'
 /* Render component */
 export const Home: React.FC = () =>
   <div className={styles['page']}>
-    <Welcome status="unavailable" nextDate="01/03/22" miniMeName="unavailable" />
+    <Welcome status="unavailable" nextDate="01/03/22" miniMeName="at-desk" />
   </div>
 
 export default Home
